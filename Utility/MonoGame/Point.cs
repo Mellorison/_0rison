4a0de18740912efd097aa52087bde740982b09ff
Daniel Tumelo <41048336+Mellorison@users.noreply.github.com>
2018-10-22 09:04:02 +0000

    Add files via upload

---
+++ Utility/MonoGame/Point.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace _0rison {
+    /// <summary>
+    /// Struct for representing a Point.
+    /// </summary>
+    public struct Point : IEquatable<Point> {
+
+        #region Private Fields
+
+        private static Point zeroPoint = new Point();
+
+        #endregion Private Fields
+
+        #region Public Fields
+
+        public int X;
+        public int Y;
+
+        #endregion Public Fields
+
+        #region Properties
+
+        public static Point Zero {
+            get { return zeroPoint; }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public Point(int x, int y) {
+            this.X = x;
+            this.Y = y;
+        }
+
+        #endregion Constructors
+
+        #region Public methods
+
+        public static bool operator ==(Point a, Point b) {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Point a, Point b) {
+            return !a.Equals(b);
+        }
+
+        public bool Equals(Point other) {
+            return ((X == other.X) && (Y == other.Y));
+        }
+
+        public override bool Equals(object obj) {
+            return (obj is Point) ? Equals((Point)obj) : false;
+        }
+
+        public override int GetHashCode() {
+            return X ^ Y;
+        }
+
+        public override string ToString() {
+            return string.Format("{{X:{0} Y:{1}}}", X, Y);
+        }
+
+        #endregion
+
+    }
+}
+
+
